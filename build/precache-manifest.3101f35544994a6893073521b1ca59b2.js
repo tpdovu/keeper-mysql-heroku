b4575d2e37e7558825f6954a26724ce40131a7c2
Tony DoVu <tpdovu@gmail.com>
2021-09-08 16:06:17 -0500

    updated git ignore

---
+++ build/precache-manifest.3101f35544994a6893073521b1ca59b2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb6607d1849e07693c2d76d4f795d3da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6edfca92938c3115cc06",
+    "url": "/static/js/2.7ef33b1e.chunk.js"
+  },
+  {
+    "revision": "e9f5981bbd446386a5b1",
+    "url": "/static/js/main.7ef1827c.chunk.js"
+  },
+  {
+    "revision": "583e51a7281672351041",
+    "url": "/static/js/runtime-main.38f647cd.js"
+  }
+]);
