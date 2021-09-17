06fb64ad17617efbbed95d99d4de1ffefc11c7ae
Tony DoVu <tpdovu@gmail.com>
2021-09-17 18:01:42 -0500

    added notes page

---
+++ src/components/NotesPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotesPage() {
+  return (
+    <div className="container">
+      <h2>Notes</h2>
+      <p>Create, read update and delete notes here!</p>
+    </div>
+  );
+}
+
+export default NotesPage;
